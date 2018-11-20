c8da21689d365c7a15f848411670572b003aada5
Domenger <domenger@MacBook-Pro-de-Domenger.local>
2018-11-21 00:47:22 +0100

    sphere

--- graphe.js
+++ graphe.js
@@ -42,36 +42,46 @@ function init() {
     scene.add(light);
     group = new THREE.Group();
     scene.add(group);
+    
     var geometries = [
 					new THREE.BoxBufferGeometry(0.2, 0.2, 0.2),
 					new THREE.ConeBufferGeometry(0.2, 0.2, 64),
 					new THREE.CylinderBufferGeometry(0.2, 0.2, 0.2, 64),
-					new THREE.IcosahedronBufferGeometry(0.2, 3),
+					new THREE.IcosahedronBufferGeometry(0.05, 5),
 					new THREE.TorusBufferGeometry(0.2, 0.04, 64, 32)
 				];
-    for (var i = 0; i < 50; i++) {
-        var geometry = geometries[Math.floor(Math.random() * geometries.length)];
+    
+    for (var i = 0; i < 100; i++) {
+        
+        //creation de spheres
+        var geometry = geometries[3];
+        
         var material = new THREE.MeshStandardMaterial({
             color: Math.random() * 0xffffff,
             roughness: 0.7,
             metalness: 0.0
         });
+        
         var object = new THREE.Mesh(geometry, material);
+        
         object.position.x = Math.random() * 4 - 2;
         object.position.y = Math.random() * 2;
         object.position.z = Math.random() * 4 - 2;
         object.rotation.x = Math.random() * 2 * Math.PI;
         object.rotation.y = Math.random() * 2 * Math.PI;
         object.rotation.z = Math.random() * 2 * Math.PI;
+        
         object.scale.setScalar(Math.random() + 0.5);
         object.castShadow = true;
         object.receiveShadow = true;
         group.add(object);
     }
-    //
+    
+    
     renderer = new THREE.WebGLRenderer({
         antialias: true
     });
+    
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.gammaInput = true;
@@ -80,6 +90,7 @@ function init() {
     renderer.vr.enabled = true;
     container.appendChild(renderer.domElement);
     document.body.appendChild(WEBVR.createButton(renderer));
+    
     // controllers
     controller1 = renderer.vr.getController(0);
     controller1.addEventListener('selectstart', onSelectStart);
@@ -89,6 +100,7 @@ function init() {
     controller2.addEventListener('selectstart', onSelectStart);
     controller2.addEventListener('selectend', onSelectEnd);
     scene.add(controller2);
+    
     //
     var geometry = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, -1)]);
     var line = new THREE.Line(geometry);
