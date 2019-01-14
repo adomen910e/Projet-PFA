3b3184c94e92bca845f84ab3143ee0b36cf8f22a
Domenger <domenger@MacBook-Pro-de-Domenger.local>
2019-01-14 23:16:46 +0100

    new essaie

--- PROJET-GEARVR-2.0/graphe.js
+++ PROJET-GEARVR-2.0/graphe.js
@@ -30,7 +30,7 @@ function init() {
     scene.background = new THREE.Color(0x000000);
 
 
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 10);
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 10000);
 
     scene.add(new THREE.HemisphereLight(0x808080, 0x606060));
 
