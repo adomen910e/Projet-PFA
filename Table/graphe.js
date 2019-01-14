91feec25c54d36c7614e4b6e878ac12272729a2b
Domenger <domenger@MacBook-Pro-de-Domenger.local>
2019-01-15 00:12:02 +0100

    erase error

--- Table/graphe.js
+++ Table/graphe.js
@@ -233,9 +233,9 @@ function intersectObjects(controller) {
         var object = intersection.object;
 
         if (object.type = "no") {
-            erase_other(object);
-            move_front_camera(object);
-            
+//            erase_other(object);
+//            move_front_camera(object);
+//            
         } else {
             object.material.emissive.r = 1;
             intersected.push(object);
