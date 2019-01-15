f6ee734925952ebf1bbee92f6406860b4590914c
Domenger <domenger@MacBook-Pro-de-Domenger.local>
2019-01-15 01:59:13 +0100

    test no move

--- Table/graphe.js
+++ Table/graphe.js
@@ -200,6 +200,7 @@ function onSelectStart(event) {
             controller.add(object);
             controller.userData.selected = object;
         } else {
+            erase_other(object);
             controller.userData.selected = object;
         }
     }
